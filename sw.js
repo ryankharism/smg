32f84740198c0977a64b9b6c784ba1f9a377f3d0
ryankharism <ryankharism@gmail.com>
2026-03-16 14:58:56 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,39 @@
-const CACHE_NAME = 'presensi-farm-v1';
-const urlsToCache = ['./', 'index.html', 'manifest.json'];
+const CACHE_NAME = 'presensi-farm-v2'; // Ubah ke v2 agar browser tahu ada update
+const urlsToCache = [
+  './',
+  'index.html',
+  'manifest.json',
+  'icon/icon-192.png',
+  'icon/icon-512.png'
+];
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Membuka cache dan menyimpan icon baru...');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Menghapus cache lama agar tidak memenuhi memori HP
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Menghapus cache lama:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
