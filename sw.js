b421215da54293b501ea9feed0df37bb2348996d
ryankharism <ryankharism@gmail.com>
2026-03-16 09:49:47 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = 'presensi-farm-v1';
+const urlsToCache = ['./', 'index.html', 'manifest.json'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
